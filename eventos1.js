3bd2af56171fe3d2c2a8e6b805c6403ad25c65a6
Marta S Santos <109400186+MartaSSantos@users.noreply.github.com>
2023-04-07 01:43:27 -0300

    colocando animacao no faq

--- eventos1.js
+++ eventos1.js
@@ -1,19 +1,42 @@
-const tabmenu = document.querySelectorAll('.js-tabmenu li');
-const contet = document.querySelectorAll('.js-contet section');
 
-if (tabmenu.length && contet.length) {
-    contet[0].classList.add('ativo')
+// Protegendo o codigo já feito!
+function initTabNav() {
+    const tabmenu = document.querySelectorAll('.js-tabmenu li');
+    const contet = document.querySelectorAll('.js-contet section');
 
-    function activeTab(index) {
-        contet.forEach((section) => {
-            section.classList.remove('ativo');
-        })
-        contet[index].classList.add('ativo');
-    }
+    if (tabmenu.length && contet.length) {
+        contet[0].classList.add('ativo')
 
-    tabmenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
+        function activeTab(index) {
+            contet.forEach((section) => {
+                section.classList.remove('ativo');
+            })
+            contet[index].classList.add('ativo');
+        }
+
+        tabmenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-    });
+    }
 }
+
+initTabNav();
+
+function initAccordio() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClas = 'ativo';
+    if (accordionList.length) {
+        accordionList[0].classList.add(activeClas)
+        accordionList[0].nextElementSibling.classList.add(activeClas);
+        function activeAcordion() {
+            this.classList.toggle(activeClas);
+            this.nextElementSibling.classList.toggle(activeClas);
+        }
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAcordion);
+        });
+    }
+}
+initAccordio();
