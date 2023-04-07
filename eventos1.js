490f1a185d1273afd8faa9953f02325eed18db56
Marta S Santos <109400186+MartaSSantos@users.noreply.github.com>
2023-04-07 02:29:49 -0300

    mudaças

--- eventos1.js
+++ eventos1.js
@@ -40,3 +40,23 @@ function initAccordio() {
     }
 }
 initAccordio();
+
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu [href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href')
+        const section = document.querySelector(href)
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+}
+
+initScrollSuave();
